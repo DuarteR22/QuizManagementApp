73f36e94ca043ff4aba99bd5c057e58a2cf6f2ed
Duarte <duartebramos@gmail.com>
2025-11-03 15:20:31 +0000

    Criação de classes

---
+++ Projeto/app/src/main/java/com/example/quizmanagementapp/Quiz.kt
@@ -0,0 +1,6 @@
+package com.example.quizmanagementapp
+
+class Quiz(val titulo : String, val descricao : String, val tempoMax : Int) {
+
+
+}
---
+++ app/src/main/java/com/example/quizmanagementapp/Questao.kt
@@ -0,0 +1,4 @@
+package com.example.quizmanagementapp
+
+class Questao(val pergunta : String, val numRespostas : Int, val respostas: List<String>, val respostaCorreta: Int) {
+}
